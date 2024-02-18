64852010b39453abcf3dc514a8aa28adb65d9405
srz_zumix <zumix.cpp@gmail.com>
2024-02-19 01:35:33 +0900

    up

--- src/post/index.ts
+++ src/post/index.ts
@@ -10,7 +10,7 @@ const exec = require('@actions/exec');
 async function run() {
   try {
     const content = core.getInput('post-run', { required: true })
-    const scriptPath = process.env.RUNNTER_TEMP + "/post-run.sh"
+    const scriptPath = process.env.RUNNER_TEMP + "/post-run.sh"
     await fs.writeFile(scriptPath, content)
     const bashPath = await io.which('bash', true)
     await exec.exec(`"${bashPath}"`, [scriptPath]);
