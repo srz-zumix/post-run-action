d48a906dd38e5548e8edefd938da02535457a108
srz_zumix <zumix.cpp@gmail.com>
2024-02-19 16:32:57 +0900

    first (#2)

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,5 @@
 /**
  * The entrypoint for the action.
  */
-const { run } = require('./main')
 
-run()
+console.log('post-run-action')
---
+++ src/post/index.ts
@@ -0,0 +1,24 @@
+/**
+ * The entrypoint for the post action.
+ */
+
+import * as core from '@actions/core'
+import { promises as fs } from 'fs'
+import * as io from '@actions/io'
+import * as exec from '@actions/exec'
+
+async function run(): Promise<void> {
+  try {
+    const content = core.getInput('post-run', { required: true })
+    const runnerTempPath: string = process.env.RUNNER_TEMP as string
+    const scriptPath = `${runnerTempPath}/post-run.sh`
+    await fs.writeFile(scriptPath, content)
+    const bashPath: string = await io.which('bash', true)
+    await exec.exec(`"${bashPath}"`, [scriptPath])
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+void run()
