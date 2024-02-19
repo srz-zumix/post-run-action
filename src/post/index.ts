4690472d9b296936b684d590a2730e9b0861b621
srz_zumix <zumix.cpp@gmail.com>
2024-02-19 16:58:41 +0900

    shell option

--- src/post/index.ts
+++ src/post/index.ts
@@ -7,14 +7,35 @@ import { promises as fs } from 'fs'
 import * as io from '@actions/io'
 import * as exec from '@actions/exec'
 
+async function resolveShell(): Promise<string[]> {
+  const defaultCommands: {[key: string]: string[]}  = {
+    'default': [ 'bash', '-e', '{0}' ],
+    'bash': [ 'bash', '--noprofile', '--norc', '-eo', 'pipefail', '{0}' ]
+  }
+  const shellCommand: string = core.getInput('shell', { required: false })
+  let shellCommands = shellCommand.split(' ')
+  if( shellCommands.length == 0 ) {
+    shellCommands = defaultCommands['default']
+  }
+  if( shellCommands.length == 1 ) {
+    shellCommands = defaultCommands[shellCommands[0]]
+  }
+  return shellCommands
+}
+
 async function run(): Promise<void> {
   try {
-    const content = core.getInput('post-run', { required: true })
+    const content: string = core.getInput('post-run', { required: true })
+    const shellCommands: string[] = await resolveShell()
+    const commandPath: string = await io.which(shellCommands[0], true)
+      
     const runnerTempPath: string = process.env.RUNNER_TEMP as string
     const scriptPath = `${runnerTempPath}/post-run.sh`
+
+    const commandArgs = shellCommands.slice(1).map((item) => item === '{0}' ? scriptPath : item)
+
     await fs.writeFile(scriptPath, content)
-    const bashPath: string = await io.which('bash', true)
-    await exec.exec(`"${bashPath}"`, [scriptPath])
+    await exec.exec(commandPath, [scriptPath])
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
