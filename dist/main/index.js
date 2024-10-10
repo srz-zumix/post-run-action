96b20921ae83662f9286e0a087b0c267632eb814
srz_zumix <zumix.cpp@gmail.com>
2024-10-11 00:24:02 +0900

    ts

--- dist/main/index.js
+++ dist/main/index.js
@@ -1,15 +1,22 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
 /**
  * The entrypoint for the action.
  */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+console.log('post-run-action');
 
-console.log('post-run-action')
+})();
 
 module.exports = __webpack_exports__;
 /******/ })()
