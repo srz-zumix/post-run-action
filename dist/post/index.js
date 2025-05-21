19012740d0bc054c45f1eb7f09fed32b70f4e55a
srz_zumix <zumix.cpp@gmail.com>
2025-05-21 11:03:19 +0900

    fix

--- dist/post/index.js
+++ dist/post/index.js
@@ -286,11 +286,11 @@ function requireProxy () {
 	    })();
 	    if (proxyVar) {
 	        try {
-	            return new URL(proxyVar);
+	            return new DecodedURL(proxyVar);
 	        }
 	        catch (_a) {
 	            if (!proxyVar.startsWith('http://') && !proxyVar.startsWith('https://'))
-	                return new URL(`http://${proxyVar}`);
+	                return new DecodedURL(`http://${proxyVar}`);
 	        }
 	    }
 	    else {
@@ -348,6 +348,19 @@ function requireProxy () {
 	        hostLower.startsWith('127.') ||
 	        hostLower.startsWith('[::1]') ||
 	        hostLower.startsWith('[0:0:0:0:0:0:0:1]'));
+	}
+	class DecodedURL extends URL {
+	    constructor(url, base) {
+	        super(url, base);
+	        this._decodedUsername = decodeURIComponent(super.username);
+	        this._decodedPassword = decodeURIComponent(super.password);
+	    }
+	    get username() {
+	        return this._decodedUsername;
+	    }
+	    get password() {
+	        return this._decodedPassword;
+	    }
 	}
 	
 	return proxy;
@@ -2182,7 +2195,7 @@ function requireDicer () {
 	  if (this._headerFirst && this._isPreamble) {
 	    if (!this._part) {
 	      this._part = new PartStream(this._partOpts);
-	      if (this._events.preamble) { this.emit('preamble', this._part); } else { this._ignore(); }
+	      if (this.listenerCount('preamble') !== 0) { this.emit('preamble', this._part); } else { this._ignore(); }
 	    }
 	    const r = this._hparser.push(data);
 	    if (!this._inHeader && r !== undefined && r < data.length) { data = data.slice(r); } else { return cb() }
@@ -2239,7 +2252,7 @@ function requireDicer () {
 	      }
 	    }
 	    if (this._dashes === 2) {
-	      if ((start + i) < end && this._events.trailer) { this.emit('trailer', data.slice(start + i, end)); }
+	      if ((start + i) < end && this.listenerCount('trailer') !== 0) { this.emit('trailer', data.slice(start + i, end)); }
 	      this.reset();
 	      this._finished = true;
 	      // no more parts will be added
@@ -2257,7 +2270,13 @@ function requireDicer () {
 	    this._part._read = function (n) {
 	      self._unpause();
 	    };
-	    if (this._isPreamble && this._events.preamble) { this.emit('preamble', this._part); } else if (this._isPreamble !== true && this._events.part) { this.emit('part', this._part); } else { this._ignore(); }
+	    if (this._isPreamble && this.listenerCount('preamble') !== 0) {
+	      this.emit('preamble', this._part);
+	    } else if (this._isPreamble !== true && this.listenerCount('part') !== 0) {
+	      this.emit('part', this._part);
+	    } else {
+	      this._ignore();
+	    }
 	    if (!this._isPreamble) { this._inHeader = true; }
 	  }
 	  if (data && start < end && !this._ignoreData) {
@@ -2415,7 +2434,7 @@ function requireDecodeText () {
 	    if (textDecoders.has(this.toString())) {
 	      try {
 	        return textDecoders.get(this).decode(data)
-	      } catch (e) { }
+	      } catch {}
 	    }
 	    return typeof data === 'string'
 	      ? data
@@ -2831,7 +2850,7 @@ function requireMultipart () {
 
 	        ++nfiles;
 
-	        if (!boy._events.file) {
+	        if (boy.listenerCount('file') === 0) {
 	          self.parser._ignore();
 	          return
 	        }
@@ -24837,7 +24856,7 @@ function requireLib () {
 	        if (this._keepAlive && useProxy) {
 	            agent = this._proxyAgent;
 	        }
-	        if (this._keepAlive && !useProxy) {
+	        if (!useProxy) {
 	            agent = this._agent;
 	        }
 	        // if agent is already assigned use that agent.
@@ -24869,15 +24888,11 @@ function requireLib () {
 	            agent = tunnelAgent(agentOptions);
 	            this._proxyAgent = agent;
 	        }
-	        // if reusing agent across request and tunneling agent isn't assigned create a new agent
-	        if (this._keepAlive && !agent) {
+	        // if tunneling agent isn't assigned create a new agent
+	        if (!agent) {
 	            const options = { keepAlive: this._keepAlive, maxSockets };
 	            agent = usingSsl ? new https.Agent(options) : new http.Agent(options);
 	            this._agent = agent;
-	        }
-	        // if not using private agent and tunnel agent isn't setup then use global agent
-	        if (!agent) {
-	            agent = usingSsl ? https.globalAgent : http.globalAgent;
 	        }
 	        if (usingSsl && this._ignoreSslError) {
 	            // we don't want to set NODE_TLS_REJECT_UNAUTHORIZED=0 since that will affect request for entire process
@@ -24900,7 +24915,7 @@ function requireLib () {
 	        }
 	        const usingSsl = parsedUrl.protocol === 'https:';
 	        proxyAgent = new undici_1.ProxyAgent(Object.assign({ uri: proxyUrl.href, pipelining: !this._keepAlive ? 0 : 1 }, ((proxyUrl.username || proxyUrl.password) && {
-	            token: `${proxyUrl.username}:${proxyUrl.password}`
+	            token: `Basic ${Buffer.from(`${proxyUrl.username}:${proxyUrl.password}`).toString('base64')}`
 	        })));
 	        this._proxyAgentDispatcher = proxyAgent;
 	        if (usingSsl && this._ignoreSslError) {
